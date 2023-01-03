e833cbd4144594c2690e39bb98d9f896202410e2
ismilearefin <arefinismile@gmail.com>
2023-01-03 23:59:36 +0600

    with data

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
                 </ul>
               </li>
               <li>
-                <a>Item 3</a>
+                <a>Home 4 25 </a>
               </li>
             </ul>
           </div>
