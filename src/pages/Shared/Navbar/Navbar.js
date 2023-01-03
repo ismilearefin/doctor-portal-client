bc33d87b61de904cbac2687c79be4a6f477e8628
ismilearefin <arefinismile@gmail.com>
2023-01-04 00:49:19 +0600

    navbar

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -28,36 +28,19 @@ const Navbar = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
+                <Link>Item 1</Link>
               </li>
               <li tabIndex={0}>
-                <a className="justify-between">
+                <Link className="justify-between">
                   Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                </Link>
               </li>
               <li>
-                <a>Home 4 25 </a>
+                <Link>Home</Link>
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">Doctors-Portal</a>
+          <Link className="btn btn-ghost normal-case text-xl">Doctors-Portal</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -65,34 +48,17 @@ const Navbar = () => {
               <Link>Home</Link>
             </li>
             <li tabIndex={0}>
-              <a>
+              <Link>
                 Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              </Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link>Item 3</Link>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <Link className="btn">Get started</Link>
         </div>
       </div>
     </div>
