171416d640ef9b09c32297189dd69a7ba525b557
ismilearefin <arefinismile@gmail.com>
2023-01-08 20:36:24 +0600

    Login page design

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCF-wizuG2p3k577XCPImAM3hxqsvo15KI",
+  authDomain: "doctor-portal-ac586.firebaseapp.com",
+  projectId: "doctor-portal-ac586",
+  storageBucket: "doctor-portal-ac586.appspot.com",
+  messagingSenderId: "1089165058513",
+  appId: "1:1089165058513:web:52014d4a17e6fa4d0f80f0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
     return (
-        <div>
-            I ma Login
+        <div className='flex justify-center items-center '>
+            <form onSubmit={handleSubmit(onSubmit)} className="p-5  rounded-md h-[480px] w-96" style={{"box-shadow": "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px"}}>
+            <p className='text-center text-xl font-bold'>Login</p>
+            <label className='text-sm'>Email</label>
+            <input className='w-full rounded-md border h-11' type="email" {...register("email")} />
+            <label className='text-sm'>password</label>
+            <input className='w-full rounded-md border h-11 mt-6' type="password" {...register("password")} />
+            <label className='text-sm'>Forgot password?</label>
+            <input className='w-full rounded-md border h-11 mt-8 bg-neutral text-white'  type="Submit" />
+            <p className='text-sm text-center mt-3'>New to Doctors Portal? <span className='text-secondary'>Create new account</span></p>
+            <div className='flex items-center justify-between my-4'>
+                <hr className='w-2/4 border'/>
+                <p className='mx-5'>OR</p>
+                <hr className='w-2/4 border'/>
+            </div>
+            <button className='border rounded-md w-full h-11 text-black'>CONTINUE WITH GOOGLE</button>
+            </form>
         </div>
     );
 };
